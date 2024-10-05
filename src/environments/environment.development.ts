7eb2d9bf9984e4b3fce5e093c6e3e69e7c30b5f6
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-10-06 01:19:05 +0200

    removed env vars

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  firebase: {
-    "projectId":"iyustlop-contacts",
-    "appId":"1:682855426193:web:1999e93e894bcd300c7730",
-    "storageBucket":"iyustlop-contacts.appspot.com",
-    "apiKey":process.env.API_KEY,
-    "authDomain":"iyustlop-contacts.firebaseapp.com",
-    "messagingSenderId":"682855426193"}
-};
