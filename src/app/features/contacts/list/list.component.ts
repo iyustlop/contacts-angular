e86b6975ac965d4e52bad804079a3dc51ddea667
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-10-08 10:49:51 +0200

    show all contacts using service get method

--- src/app/features/contacts/list/list.component.ts
+++ src/app/features/contacts/list/list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { GridComponent } from '@components/grid/grid.component';
 import { ColumnKeys, Contact } from '../contacts.interface';
+import { ContactService } from '../contacts.services';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -15,9 +17,24 @@ import { ColumnKeys, Contact } from '../contacts.interface';
   `,
   styles: ``
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  
   data!: Contact[];
   displayedColumns:ColumnKeys<Contact> = ['id', 'name', 'phone', 'email', 'action'];
   sortables:ColumnKeys<Contact> = ['id', 'name', 'phone', 'email'];
+  
+  ngOnInit(): void {
+    this.getAllContacts()
+  }
+
+  private readonly _contactSvc = inject(ContactService);
+  
+  getAllContacts(){
+    this._contactSvc.getAllContacts()
+    .pipe(
+      tap((contacts:Contact[]) => this.data = [...contacts])
+    )
+    .subscribe()
+  }
 
 }
